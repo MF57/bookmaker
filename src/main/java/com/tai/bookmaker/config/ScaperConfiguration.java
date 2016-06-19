18a4776b034e4f35de535f8da716e041799d4e7d
MF57 <p-bochenek@outlook.com>
2016-06-19 23:00:42 +0200

    scraper init jsoup

--- src/main/java/com/tai/bookmaker/config/ScaperConfiguration.java
+++ src/main/java/com/tai/bookmaker/config/ScaperConfiguration.java
@@ -1,7 +1,11 @@
 package com.tai.bookmaker.config;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
 
 /**
  * Created by mf57 on 19.06.2016.
@@ -9,8 +13,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 public class ScaperConfiguration {
 
-    @Scheduled(fixedDelay = 1000)
-    public void test() {
-        System.out.println("AAAA");
+    @Scheduled(initialDelay = 10000, fixedDelay = 60000)
+    public void test() throws IOException {
+        Document doc = Jsoup.connect("http://www.livescore.com/soccer/").get();
+        System.out.println(doc.toString());
     }
 }
